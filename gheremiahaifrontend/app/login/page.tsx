fffe76ef62d4e0131bc4bafab61bf5b74badfc24
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-04-17 23:26:24 +0200

    removed localstorage dependencies

--- gheremiahaifrontend/app/login/page.tsx
+++ gheremiahaifrontend/app/login/page.tsx
@@ -28,8 +28,6 @@ export default function LoginPage() {
             const data = await response.json();
 
             if (data.success) {
-                localStorage.setItem('accessToken', data.data.accessToken);
-                localStorage.setItem('refreshToken', data.data.refreshToken);
                 localStorage.setItem('user', JSON.stringify(data.data.user));
                 
                 // Redirect admin to admin page, regular users to chat
