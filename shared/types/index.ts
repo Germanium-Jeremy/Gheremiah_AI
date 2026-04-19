9fb6222ddfacb16f74bf742b4d081c6717c619bb
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-04-19 21:28:45 +0200

    modified type User to add role

--- shared/types/index.ts
+++ shared/types/index.ts
@@ -14,6 +14,7 @@ export interface User {
     email: string;
     passwordHash: string;
     subscriptionTier: SubscriptionTier;
+    role: 'user' | 'admin';
     isVerified: boolean;
     createdAt: Date;
     updatedAt: Date;
