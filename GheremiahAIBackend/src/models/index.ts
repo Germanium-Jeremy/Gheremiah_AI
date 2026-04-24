73b92df3ca3372cd89d36df7698e2191fb4dc15d
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-04-24 13:24:39 +0200

    added an exported

---
+++ GheremiahAIBackend/src/models/index.ts
@@ -0,0 +1,8 @@
+export { User } from './User';
+export type { IUserDocument } from './User';
+
+export { ApiKey } from './ApiKey';
+export type { IApiKeyDocument } from './ApiKey';
+
+export { UsageLog } from './UsageLog';
+export type { IUsageLogDocument } from './UsageLog';
